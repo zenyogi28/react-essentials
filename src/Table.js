5d251051280b0023b34c8db422e39cb94d349af5
zenyogi <yb_shah@hotmail.com>
2022-07-20 13:38:41 +1000

    Initial commit

---
+++ src/Table.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+class Table extends Component {
+    render() {
+        return (
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Job</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Charlie</td>
+                        <td>Janitor</td>
+                    </tr>
+                    <tr>
+                        <td>Mac</td>
+                        <td>Bouncer</td>
+                    </tr>
+                </tbody>
+            </table>
+        )
+    }
+}
+
+export default Table;
